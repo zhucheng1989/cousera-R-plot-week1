fcdd59d4ba1530d3942906504a4dfc2bfdb0b190
zhucheng1989 <zhucheng1989@hotmail.com>
2015-06-08 02:29:14 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,10 @@
 setwd('C:/Users/Administrator/Desktop')
 data<-read.table('household_power_consumption.txt',header=T,sep=';',
                  stringsAsFactors=F)
-data$Date<-as.Date(data$Date,format='%d/%m/%Y')
 
+data$Time<-strptime(paste(data$Date,data$Time),'%d/%m/%Y %H:%M:%S')
+data$Date<-as.Date(data$Date,format='%d/%m/%Y')
 data<-data[data$Date %in% as.Date(c('2007-02-01','2007-02-02')),]
-data$Time<-strptime(data$Time,format='%H:%M:%S')
 
 png('plot1.png',width=480,height=480)
 with(data,hist(as.numeric(Global_active_power),
